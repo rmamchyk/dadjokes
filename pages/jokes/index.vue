57f20e347c6a2c389baf92677bcc46d395793b2d
rmamchyk <roman.mamchyk@gmail.com>
2019-06-27 01:24:18 +0300

    Implemented jokes listing, search and joke detail page

--- pages/jokes/index.vue
+++ pages/jokes/index.vue
@@ -1,10 +1,68 @@
 <template>
-    <div>jokes</div>
+    <div>
+        <search-jokes @search-text="searchText" />
+         <joke v-for="joke in  jokes" 
+            :key="joke.id"
+            :id="joke.id"
+            :joke="joke.joke" />
+    </div>
 </template>
 
 <script>
-export default {
+import axios from 'axios';
+import Joke from '../../components/Joke';
+import SearchJokes from '../../components/SearchJokes';
 
+export default {
+    components: {
+        Joke,
+        SearchJokes
+    },
+    data() {
+        return {
+            jokes: []
+        }
+    },
+    async created() {
+        const config = {
+            headers: {
+                'Accept': 'application/json'
+            }
+        };
+        try {
+            const res = await axios.get('https://icanhazdadjoke.com/search', config);
+            this.jokes = res.data.results;
+        } catch (err) {
+            console.log(err);
+        }
+    },
+    methods: {
+        async searchText(text) {
+            const config = {
+                headers: {
+                    'Accept': 'application/json'
+                }
+            };
+            try {
+                const res = await axios.get(`https://icanhazdadjoke.com/search?term=${text}`, config);
+                this.jokes = res.data.results;
+            } catch (err) {
+                console.log(err);
+            }
+        }
+    },
+    head() {
+        return {
+        title: 'Dad Jokes',
+        meta: [
+            {
+            hid: 'description',
+            name: 'description',
+            content: 'Best place for corny dad jokes'
+            }
+        ]
+        }
+    }
 }
 </script>
 
